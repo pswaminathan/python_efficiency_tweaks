d76a8d46ed9a27d35d95c371b666d0ec8705cdd6
rasbt <se.raschka@me.com>
2014-01-14 02:32:57 -0500

    list_func data & plots

---
+++ plots/plot_list_func_bar.py
@@ -0,0 +1,22 @@
+# Plotting performance of list_func_.py scripts
+# bar chart of relative comparison with variances as error bars
+
+#import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
+import matplotlib.pyplot as plt
+
+performance = [1,2.31993328105027,3.45663135602093]
+variance = [0,0.00228182736026489,0.00937418320073582]
+scripts = ['list_func_1.py', 'list_func_2.py', 'list_func_3.py']
+
+x_pos = np.arange(len(scripts))
+
+plt.bar(x_pos, performance, yerr=variance, align='center', alpha=0.5)
+plt.xticks(x_pos, scripts)
+plt.ylim([0,4])
+
+plt.ylabel('rel. performance gain')
+plt.title('Lists item function call - Speed improvements')
+
+#plt.show()
+plt.savefig('list_func_bar.png')
