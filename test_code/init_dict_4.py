9ee89b90ae934fe49dbb15af999375d0a3678ff4
Sebastian Raschka <raskberry@mail.com>
2014-01-15 14:40:39 -0500

    Update init_dict_4.py

--- test_code/init_dict_4.py
+++ test_code/init_dict_4.py
@@ -7,12 +7,11 @@ orig_dict = {i:0 for i in range(1, n)}
 
 start_time = time.clock()
 
-for i in range(n-2, 2*n):
+for i in range(n-1, 2*n):
     try:
         orig_dict[i] + 1
     except KeyError:
         orig_dict[i] = 1
-
-     
+    
 elapsed_time = time.clock() - start_time
 print ("Time elapsed: {0:.4f} sec".format(elapsed_time))
